05f51cfbe63d546049217e2173859bd489727065
grantSwalwell <grant.swalwell-1@ou.edu>
2017-07-24 09:36:20 -0500

    added a lot to trainer

--- Trainer - Array Neural Net.h
+++ Trainer - Array Neural Net.h
@@ -43,25 +43,207 @@ protected:
 	// use batch learning
 	bool useBatch;
 	
+	// PROTECTED METHODS
+
+	// backpropagate errors through network
+	void backpropagate(double* desiredValues)
+	{
+		// get output error gradients and set delta output
+		for (int i = 0; i < network->NumOutput; i++)
+		{
+			// output gradoemt
+			outputGradient[i] = getOutputErrorGradient(desiredValues[i], network->OutputNodes[i]);
+
+			// for each weight update, get change based on gradient
+			for (int j = 0; j <= network->NumHidden; j++)
+			{
+				if (!useBatch) deltaOutput[j][i] = changeOutput(i, j);
+				
+				else deltaOutput[j][i] += changeOutput(i, j);
+			}
+		}
+
+		// get hidden error gradients and set delta ouput
+		for (int i = 0; i <= network->NumHidden; i++)
+		{
+			// get hidden gradient
+			hiddenGradient[i] = getHiddenErrorGradient(i);
+
+			// set delta output
+			for (int j = 0; j <= network->NumInput; j++)
+			{
+				if (!useBatch) deltaInput[j][i] = changeHidden(i, j);
+
+				else deltaInput[j][i] += changeHidden(i, j);
+			}
+		}
+
+		// if using batch, do not immediatel update weights
+		if (!useBatch) updateWeights();
+	}
+
+	// update weights according to delta
+	void updateWeights()
+	{
+		// update input weights
+		for (int i = 0; i <= network->NumInput; i++)
+		{
+			for (int j = 0; j <= network->NumHidden; j++)
+			{
+				network->InputWeights[i][j] += deltaInput[i][j];
+
+				// if using batch set delta to 0
+				if (useBatch) deltaInput[i][j] = 0;
+			}
+		}
+
+		// update outputweights
+		for (int i = 0; i <= network->NumHidden; i++)
+		{
+			for (int j = 0; j <= network->NumOutput; j++)
+			{
+				network->OutputWeights[i][j] += deltaOutput[i][j];
+
+				if (useBatch) deltaOutput[i][j] = 0;
+			}
+		}
+	}
+
+	// run a training epoch 
 
 public:
+
+	// set learning rate, momentum, and batch learning
+	void setTrainingParameters(double learningRate, double momentum, bool batch)
+	{
+		this->learningRate = learningRate;
+		this->momentum = momentum;
+		this->useBatch = batch;
+	}
+
+	// set max epochs and desired accuracy
+	void setStoppingConditions(int maxEpochs, double desiredAccuracy)
+	{
+		this->maxEpochs = maxEpochs;
+		this->desiredAccuracy = desiredAccuracy;
+	}
+
+	// use batch learning
+	void useBatchLearning(bool batch)
+	{
+		this->useBatch = batch;
+	}
+
+	// train network, takes dataset plus the number of rows to iterate through
+	void trainNetwork(int epochs, double** data)
+	{
+
+	}
 
 	// default constructor
 	Trainer()
 	{
 		network = NULL;
+
+		learningRate = 0;
+		momentum = 0;
+		epoch = 0;
+		maxEpochs = 0;
+		desiredAccuracy = 0;
+
+		deltaInput = NULL;
+		deltaOutput = NULL;
+
+		hiddenGradient = NULL;
+		outputGradient = NULL;
+
+		trainingSetAccuracy = 0;
+		validationSetAccuracy = 0;
+		generalizationSetAccuracy = 0;
+		trainingSetMSE = 0;
+		validationSetMSE = 0;
+		generalizationSetMSE = 0;
+
+		useBatch = false;
 	}
 
 	// initializer
 	Trainer(NeuralNet* net)
 	{
 		network = net;
+		
+		learningRate = 0.001;
+		momentum = 0.9;
+		epoch = 0;
+		maxEpochs = 1500;
+		desiredAccuracy = 0;
+
+		deltaInput = new double*[network->NumInput + 1];
+		deltaOutput = new double*[network->NumHidden + 1];
+
+		network->zero(deltaInput, network->NumInput + 1, network->NumHidden + 1);
+		network->zero(deltaOutput, network->NumHidden + 1, network->NumOutput);
+
+		hiddenGradient = new double[network->NumHidden];
+		outputGradient = new double[network->NumOutput];
+
+		network->zero(hiddenGradient, network->NumHidden + 1);
+		network->zero(outputGradient, network->NumOutput);
+
+		trainingSetAccuracy = 0;
+		validationSetAccuracy = 0;
+		generalizationSetAccuracy = 0;
+		trainingSetMSE = 0;
+		validationSetMSE = 0;
+		generalizationSetMSE = 0;
+
+		useBatch = false;
 	}
 
-	// 
+	// destructor
 	~Trainer()
 	{
 
+	}
+
+protected:
+
+	// get output error gradient
+	double getOutputErrorGradient(double desiredValue, double actualValue)
+	{
+		return desiredValue * (1 - desiredValue) * (desiredValue - actualValue);
+	}
+
+	// git hidden error gradient
+	double getHiddenErrorGradient(int index)
+	{
+		double weightedSum = 0;
+
+		for (int i = 0; i < network->NumOutput; i++)
+		{
+			weightedSum += network->OutputWeights[index][i] * outputGradient[i];
+		}
+
+		// return gradient
+		return network->HiddenNodes[index] * (1 - network->HiddenNodes[index]) * weightedSum;
+	}
+
+	// get change in output 
+	double changeOutput(int output, int hidden)
+	{
+		if (!useBatch) return learningRate * network->HiddenNodes[hidden] * outputGradient[output] + momentum
+			* deltaOutput[output][hidden];
+
+		else return learningRate * network->HiddenNodes[hidden] * outputGradient[output];
+	}
+
+	// get change in hidden
+	double changeHidden(int hidden, int input)
+	{
+		if (!useBatch) return learningRate * network->InputNodes[input] * hiddenGradient[hidden] + momentum
+			* deltaInput[input][hidden];
+
+		else return learningRate * network->InputNodes[input] * hiddenGradient[hidden];
 	}
 
 };
