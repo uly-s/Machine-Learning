37b36fb7b6f0a73c5b4755de79e5a497a875ddb3
grantSwalwell <grant.swalwell-1@ou.edu>
2017-08-04 00:21:33 -0500

    Update Source - deep neural net, non convolutional.cpp

--- Source - deep neural net, non convolutional.cpp
+++ Source - deep neural net, non convolutional.cpp
@@ -9,104 +9,109 @@
 using namespace std;
 
 
-void LoadTrainingData(int thousands, double*** training, double*** labels, double*** labelArrays, double** patterns, double** targets)
+// x labels, x * 784 patterns, x * 10 targets
+void LoadTrainingData(int samples, double* labels, double** patterns, double** targets)
 {
 	char dummy = ' ';
 
 	int index = 0;
 
-	for (int i = 0; i < thousands; i++)
-	{
-		training[i] = new double*[1000];
-
-		labels[i] = new double*[1000];
-
-		labelArrays[i] = new double*[1000];
-
-		for (int j = 0; j < 1000; j++)
-		{
-			training[i][j] = new double[784];
-			labels[i][j] = new double[1];
-			labelArrays[i][j] = new double[10];
-
-			labels[i][j][0] = 0;
-
-			cin >> labels[i][j][0];
-
-			for (int k = 0; k < 10; k++)
-			{
-				labelArrays[i][j][k] = 0;
-
-				if (k == labels[i][j][0])
-				{
-					labelArrays[i][j][k] = 1;
-				}
-			}
-
-			cin >> dummy;
-
-			for (int k = 0; k < 784; k++)
-			{
-				training[i][j][k] = 0;
-
-				cin >> training[i][j][k];
-
-				if (training[i][j][k] > 0) training[i][j][k] = 1;
-
-				if (k < 783) cin >> dummy;
-
-			}
-		}
-	}
-
-
-}
-
-
-int main()
-{
-	double*** training = new double**[50];
-
-	double*** labels = new double**[50];
-
-	double*** labelArrays = new double**[50];
-
-	double** patterns = new double*[5000];
-
-	double** targets = new double*[5000];
-
-	/*
-	for (int i = 0; i < 3000; i++)
+	for (int i = 0; i < samples; i++)
 	{
 		patterns[i] = new double[784];
 		targets[i] = new double[10];
+		labels[i] = 0;
 
-		for (int j = 0; j < 784; j++)
-		{
-			patterns[i][j] = 0;
-		}
+		// get label
+		cin >> labels[i];
+
+		// get comma
+		cin >> dummy;
 
 		for (int j = 0; j < 10; j++)
 		{
 			targets[i][j] = 0;
+
+			// if index = label this is the target
+			if (j == labels[i]) targets[i][j] = 1;
 		}
-	}*/
 
-	LoadTrainingData(2, training, labels, labelArrays, patterns, targets);
+		for (int j = 0; j < 784; j++)
+		{
+			patterns[i][j] = 0;
 
-	DeepNet* deepnet = new DeepNet(784, 28, 112, 224, 10);
+			// get pattern
+			cin >> patterns[i][j];
+
+			// get comma if we aren't at the end of the line
+			if (j < 783) cin >> dummy;
+		}
+	}
+
+}
+
+// shuffle training data
+void Shuffle(int samples, double* labels, double** patterns, double** targets)
+{
+
+}
+
+// swap two array elements
+void swap(double& A, double& B)
+{
+	// store the value of A
+	double C = A;
+
+	// set A to B
+	A = B;
+
+	// set B to C
+	B = C;
+}
+
+// get a random index in a certain range
+int randomIndex(int order)
+{
+	return (int) (rand() % order);
+}
+
+
+
+
+int main()
+{
+
+	// digits
+	double* labels = new double[5000];
+
+	// 28 * 28 patterns of images
+	double** patterns = new double*[5000];
+
+	// target arrays 0 - 9
+	double** targets = new double*[5000];
+
+	// read in x training samples 
+	//LoadTrainingData(2000, labels, patterns, targets);
+
+	DeepNet* deepnet = new DeepNet(784, 112, 56, 10);
+
+	deepnet->InitializeWeights();
 	
 	Trainer* trainer = new Trainer(deepnet);
 
-	deepnet->InitializeWeights();
-
 	trainer->Parameters(25, 1.0, 90, 5000);
 	
-	trainer->Train(training[0], labelArrays[0], 1000);
+	//trainer->Train(patterns, targets, 1000);
 
-	cout << deepnet->getSetAccuracy(training[1], labelArrays[1], 1000, 0) << endl;
+	//cout << deepnet->getSetAccuracy(patterns, targets, 1000, 999) << endl;
 
-	//cout << deepnet->getSetAccuracy(training[3], labelArrays[3], 1000) << endl;
+	int order = 10;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << randomIndex(order) << endl;
+	}
+
 
 	return 0;
 }
