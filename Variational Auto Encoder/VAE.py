0cf7b1e87117a5bfbebd9702d371c6836baf0329
grantSwalwell <grant.swalwell-1@ou.edu>
2018-06-14 16:58:16 -0500

    Finished up VAE

--- Variational Auto Encoder/VAE.py
+++ Variational Auto Encoder/VAE.py
@@ -1,76 +1,118 @@
+from keras.backend import square, exp, log, shape, random_normal
 from keras import backend as K
-from keras.engine.topology import Layer
-from keras.models import Model, Sequential
-from keras.layers import Input, Multiply, Add, Dense, Lambda
-import numpy
+from keras.models import Model
+from keras.layers import Input, Dense, Lambda
+from numpy import array, zeros
+from numpy.random import randint
+from tensorflow import reduce_mean as mean, reduce_sum as sum
+import tensorflow as tf
+
+
+
+def Data():
+
+    data_file = open("mnist_data.txt", 'r', encoding='utf-8')
+
+    data = []
+
+    for line in data_file:
+        entry = array(line.split(), dtype=float)
+        data.append(entry)
+
+    x = zeros((len(data), 784))
+
+    for i, entry in enumerate(data):
+        x[i] = entry
+
+    # normalize inputs
+    x = x / 255
+
+    return x
+
+X = Data()
+
+epochs, batch_size, samples = 100, 64, X.shape[0]
+batches = int(samples / batch_size)
+
+input, latent, hidden = 784, 256, 512
+
+# input for encoder
+
+x =  Input(shape=(input,))
+
+# encoder model
+
+hq = Dense(latent, activation='relu', kernel_initializer='glorot_normal')(x)
+mu = Dense(latent, kernel_initializer='glorot_normal')(hq)
+sigma = Dense(latent, kernel_initializer='glorot_normal')(hq)
+
+# reparameterization
+
+z = Lambda(lambda n: n[0] + exp(n[1] / 2) * random_normal(shape(n[0]), mean=0, stddev=1))([mu, sigma])
+
+# decoder model
+
+hz = Dense(hidden, input_dim=latent, activation='relu', kernel_initializer='glorot_normal')(z)
+y = Dense(input, activation='sigmoid', kernel_initializer='glorot_normal')(hz)
+
+# VAE model
+
+VAE = Model(x, y)
+
+# reconstruction loss
+
+reconstruction_loss = -sum(x * log(y) + (1 - x) * log(1 - y), 1)
+
+# KL divergence
+
+KL_div = -0.5 * sum(1 + sigma - square(mu) - exp(sigma), 1)
+
+# combined loss
+
+loss = mean(reconstruction_loss + KL_div)
+
+# optimizer
+
+Adam = tf.train.AdamOptimizer
+opt = Adam(0.0001, 0.99)
+min = opt.minimize(loss)
+
+# initializer
+
+init_global = tf.global_variables_initializer()
+init_local = tf.local_variables_initializer()
+
+# session
+
+with tf.Session() as sess:
+
+    sess.run(init_global)
+    sess.run(init_local)
+
+    _batch = zeros((batch_size, input), dtype=float)
+
+    for epoch in range(epochs):
+
+        avg = 0.0
+
+        for batch in range(batches):
+
+            for sample in range(batch_size):
+
+                _batch[sample] = X[randint(0, samples)]
+
+            _, batch_loss = sess.run([min, loss], feed_dict={x : _batch})
+
+            avg += batch_loss
+
+        avg /= batches
+
+        print(avg)
 
 
 
 
-def nll(y_true, y_pred):
-    return K.sum(K.binary_crossentropy(y_true, y_pred), axis=-1)
 
-
-
-class KL(Layer):
-    def __init__(self, *args, **kwargs):
-        super(KL, self).__init__(*args, **kwargs)
-
-    def call(self, inputs):
-        mu, log = inputs
-        batch = -0.5 * K.sum(1 + log - K.square(mu) - K.exp(log), axis=-1)
-        self.add_loss(K.mean(batch), inputs=inputs)
-
-        return inputs
-
-
-
-def VariationalAutoEncoder(input, intermediate, latent):
-
-    ### ENCODER ###
-
-    x = Input(shape=(input,))
-
-    h = Dense(intermediate, activation='relu')(x)
-
-    ### MEAN AND LOG OF VARIANCE
-
-    mu = Dense(latent)(h)
-
-    log = Dense(latent)(h)
-
-    ### KL DIVERGENCE LAYER, TAKES MEAN AND LOG VARIANCE
-
-    mu, log = KL()([mu, log])
-
-    ### STANDARD DEVIATION
-
-    sigma = Lambda(lambda t: K.exp(0.5*t))(log)
-
-    ### EPSILON FOR GAUSSIAN NOISE TERM
-
-    eps = Input(tensor=K.random_normal(shape=(K.shape(x)[0], latent)))
-
-    ### MULTIPLY STD DEV AND NOISE
-
-    zeps = Multiply()([sigma, eps])
-
-    ### OUTPUT  OF ENCODER, MEAN PLUS NOISY SIGMA
-
-    z = Add()([mu, zeps])
-
-    ### DECODER ###
-
-    decoder = Sequential([Dense(intermediate, input_dim=latent, activation='relu'),
-                          Dense(input, activation='sigmoid')])(z)
-
-    ### MODEL ###
-
-    VAE = Model(inputs=[x, eps], outputs=decoder)
-    VAE.compile(optimizer='rmsprop', loss=nll)
-
-
-    return VAE
 
 
 
